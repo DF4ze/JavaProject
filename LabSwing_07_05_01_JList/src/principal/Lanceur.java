8b0869ed6f0e6465409698d2d82da2bc5720eeb8
DF4ze <df4ze@free.fr>
2014-10-01 23:55:26 +0200

    ClassQuiSertARienBranch

--- LabSwing_07_05_01_JList/src/principal/Lanceur.java
+++ LabSwing_07_05_01_JList/src/principal/Lanceur.java
@@ -5,6 +5,8 @@ import fenetres.FenetreChoix;
 public class Lanceur {
 	public static void main(String[] args) {
 		
+		new ClassQuiSertARienBranche();
+		
 		String sText = "Choisir...Un choix...";
 		if( args.length != 0 ){
 			sText = args[0];
